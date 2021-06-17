0584d82932019a47c9a3c117f7335de7590bb101
rahadianrais1 <rahadian.rais@compnet.co.id>
2021-06-17 17:08:40 +0700

    First Commit

---
+++ String.py
@@ -0,0 +1,10 @@
+val = str(input("nama perusahaan : "))
+name = "Compnet"
+message = "Nama Perusahaan Kita" 
+message2 = "Bukan nama perusahaan kita"
+print(val)
+if val == name:
+    print(message)
+else:
+        print(message2)
+
---
+++ range 1-10.py
@@ -0,0 +1,4 @@
+count = 0
+while (count < 11):
+    print(count)
+    count = count + 1
